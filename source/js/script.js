0029998e4b277289373bfd090ffa30e15fa09add
Anna <annabananna223322@gmail.com>
2020-05-04 18:19:11 +0300

    11. Завершаем вёрстку личного проекта

--- source/js/script.js
+++ source/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   navMain.classList.remove('main-nav--nojs');
 
-  navToggle.addEventListener('click', function() {
+  navToggle.addEventListener('click', function () {
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
@@ -13,4 +13,43 @@ document.addEventListener("DOMContentLoaded", function () {
       navMain.classList.remove('main-nav--opened');
     }
   });
+
+  // Функция ymaps.ready() будет вызвана, когда
+  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+  ymaps.ready(init);
+
+  function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [55.76, 37.64],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 12
+    }),
+
+      myPlacemark1 = new ymaps.Placemark([55.76, 37.64], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/map-pin.png',
+        iconImageSize: [113, 106],
+        iconImageOffset: [-57, -106],
+      });
+
+    myMap.geoObjects.add(myPlacemark1);
+
+    if (window.innerWidth < 1200) return;
+
+    var pixelCenter = myMap.getGlobalPixelCenter([55.76, 37.64]);
+
+    pixelCenter = [
+      pixelCenter[0] - 340,
+      pixelCenter[1] - 55
+    ];
+
+    var geoCenter = myMap.options.get('projection').fromGlobalPixels(pixelCenter, myMap.getZoom());
+    myMap.setCenter(geoCenter);
+  }
 });
